59c32995ebe925f487b6fa1d144b5e7af72154ac
weipa7 <hajime0578@gmail.com>
2022-05-05 16:05:25 +0900

    second

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HelloController extends Controller
+{
+   public function index(Request $request)
+   {
+       dd($request);
+       $user = Auth::user();
+       $sort = $request->sort;
+       $param = [
+           'sort' => $sort,
+           'user' => $user
+       ];
+       return view('top.index', $param);
+   }
+}
