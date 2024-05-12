11a14bfc626c6e814ac4cc7a0d051b80436583ff
weipa7 <hfuob@yahoo.co.jp>
2024-05-12 09:03:31 +0900

    RequestMapping追加

--- src/main/java/com/example/demo/KintaiController.java
+++ src/main/java/com/example/demo/KintaiController.java
@@ -17,8 +17,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RequestMapping("")
 @Controller
 public class KintaiController {
 	@Autowired
